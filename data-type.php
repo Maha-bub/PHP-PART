e709e9dc3b9fc944e3f919e4308b7cf5e369ca84
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-03-05 10:53:43 +0600

    php-data-types

---
+++ data-type.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <?php
+    // Scaler(stirng,boolean,integer,float)
+    // Compound datatype(array, function, object)
+
+    // string
+    $data = "Hello";
+    echo $data;
+    echo "<br>";
+    var_dump($data);
+
+
+    //boolean
+    $vtype= true;
+    echo "<br>";
+    var_dump($vtype);
+
+
+    //interger
+    $int = 33;
+    echo "<br>";
+    var_dump($int);
+
+
+    //float
+     $f= 1.33;
+    echo "<br>";
+    var_dump($f);
+
+    ?>
+</body>
+</html>
