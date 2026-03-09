067926b6ee8f808a5aaefccba6ada548f7786795
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-09 16:45:46 +0600

    localGlobal

---
+++ localGlobal.php
@@ -0,0 +1,34 @@
+ <?php
+    // declaire global variable outsite from function;
+    $num=50;
+    $num2=60;
+    function multiplication(){
+        global $num;
+        global $num2; 
+        $multiply=$num*$num2;
+        echo $multiply;
+    }
+    multiplication();
+    echo("<br>");
+
+    function addition(){
+        // num1 is declaired as local variable
+        $num1 =6;
+        global $num;
+        $num++;
+        $summation =$num1+$num;
+
+        echo $summation;
+    }
+    addition();
+    echo("<br>");
+    addition();
+    echo("<br>");
+    addition();
+
+
+    // static variable
+
+
+
+    ?>
