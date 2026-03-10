b81050832b9cf22fdf5c8caede56d02f1d6b1700
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-10 10:56:48 +0600

    Passing argument by reference

--- home-1.php
+++ home-1.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
+
 <body>
-   
-   <?php
-    $sum1 = 34;
-    $sum2 = 5;
-    $sum = $sum1 + $sum2; 
 
-    echo $sum;
+    <?php
+
+    // PHP Passing Arguments by Reference Function
 
 
-    ?> 
+    function addition(&$value)
+    {
+        $sum = 0;
+
+        $sum += $value;
+    }
+    $number = 5;
+    $result = addition($number);
+    echo $number;
+
+    ?>
+
 </body>
+
 </html>
