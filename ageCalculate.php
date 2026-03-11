fc72d26da27ead1250963c65e55b3ffa620001c1
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-11 11:40:39 +0600

    Calculate age from birthdat and current date using (date_diffarecnce) Function and print output as a good formate

--- ageCalculate.php
+++ ageCalculate.php
@@ -3,12 +3,6 @@ $birthday= date_create("18-03-2003");
 $currentday= date_create("10-01-2026");
 echo "<br>";
 $age=date_diff($birthday, $currentday);
-
-echo $age->d;
-echo "<br>";
-echo $age->m;
-echo "<br>";
-echo $age->y;
 echo "<br>";
 
 echo $age->format("%d Days %m Months %y Years");
