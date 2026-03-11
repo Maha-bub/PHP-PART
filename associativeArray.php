29f07437f40e2c35a6964c3f2bc8b9bd8e7bc6c4
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-11 11:44:26 +0600

    accociative array in php

--- associativeArray.php
+++ associativeArray.php
@@ -1,16 +1,13 @@
 <?php
 $FavColor = array(
-    "turna"=> "pink",
-    "Liyes"=> "blue",
-    "Meem"=> "black",
-    "Sumi"=> "navy",
-    "anita"=> "skybl",);
+    "turna" => "pink",
+    "Liyes" => "blue",
+    "Meem" => "black",
+    "Sumi" => "navy",
+    "anita" => "skybl",
+);
 
-    echo $FavColor['turna'];
-    // foreach ($FavColor as $color => $value) {
-    //     echo $value;
-    //     echo "<br>";
-    // }
-
-
-?>
+foreach ($FavColor as $color => $value) {
+    echo $value;
+    echo "<br>";
+}
