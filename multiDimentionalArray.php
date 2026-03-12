eb3340645acae76d7adf56039ae52abd316c1cdc
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-12 10:07:52 +0600

    2D array in php

--- multiDimentionalArray.php
+++ multiDimentionalArray.php
@@ -1,15 +1,32 @@
 <?php
-$students = [
-    ["Rahim", 20, "Dhaka"],
-    ["Karim", 22, "Khulna"],
-    ["Salam", 19, "Chittagong"]
+// $students = [
+//     ["Rahim", 20, "Dhaka"],
+//     ["Karim", 22, "Khulna"],
+//     ["Salam", 19, "Chittagong"]
+// ];
+
+
+// for ($i = 0; $i < count($students); $i++) {
+//     for ($j = 0; $j < count($students[$i]); $j++) {
+//         echo $students[$i][$j] . " ";
+//     }
+//     echo "<br>";
+// }
+
+$char=[
+    ["A","E","I","O","U"],
+    ["sumi","turu","parvin","Meem","Sadiya"],
+    ["1","2","3","3","10"]
 ];
+for($i=0; $i<count($char);$i++){
+    for($j=0;$j<count($char[$i]);$j++){
+        echo $char[$i][$j]." ";
+        
 
 
-for ($i = 0; $i < count($students); $i++) {
-    for ($j = 0; $j < count($students[$i]); $j++) {
-        echo $students[$i][$j] . " ";
     }
-    echo "<br>";
+    echo " <br>";
 }
+
+?>
 
