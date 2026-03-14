f0a9ab8b193a23554d2ecba122833952b75a4c62
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-14 11:48:18 +0600

    accociative function

--- forEach_associativeArray.php
+++ forEach_associativeArray.php
@@ -1,9 +1,12 @@
 <?php
 echo "Welcome to associates array and get values using forEach function";
-$FavColor = array("Sobuj"=>"green","Rohan"=>"red","Ashik"=>"Blue","Mahabub"=>"White","Joy"=>"SKyblue") ;
-foreach($FavColor as $person => $value){
-echo "<br>Favorite color of $person is $value";
+$FavColor = array(
+    "Sobuj" => "green",
+    "Rohan" => "red",
+    "Ashik" => "Blue",
+    "Mahabub" => "White",
+    "Joy" => "SKyblue"
+);
+foreach ($FavColor as $person => $value) {
+    echo "<br>Favorite color of $person is $value";
 }
-
-
-?>
