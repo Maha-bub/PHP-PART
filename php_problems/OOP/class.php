f2379ab8aa9c155276a1bd6fdab48cb589474954
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-28 11:05:31 +0600

    basic oop concept in php using class

--- php_problems/OOP/class.php
+++ php_problems/OOP/class.php
@@ -10,10 +10,20 @@ class car{
         echo "Hi, I'm Mahabub Alam.";
     }
 
+    //Change property value
+    function change_color($color){
+        $this->color=$color;
+        return $this->color;
+
+    } 
+
 }
 
 $class_object= new car();
 echo $class_object->color;
 echo "<br>";
 echo $class_object->indentity();
+echo "<br>";
+echo $class_object->change_color("black")
+
 ?>
