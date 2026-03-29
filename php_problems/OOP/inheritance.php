521ad22f0ea8e9858571954226c94e930d1bdf2f
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-29 10:28:21 +0600

    basic concept of inheritence

--- php_problems/OOP/inheritance.php
+++ php_problems/OOP/inheritance.php
@@ -6,14 +6,27 @@
     public $address;
     public $id;
     public $subject;
-    public function StudentInfo(){
-        
-        echo "hello Student";
+    public function StudentInfo($name){
+        echo "My name is".$this->name;
+    }
+    public fu function __construct(){
+        echo "Hello world";
+    }
+    public function __destruct(){
+        echo "<br> Good bye";
+
+    }
+ }
+ class teacher extends Students{
+    public $exerience;
+    public fu nction teacherInfo(){
+        echo "I am a teacher";
     }
  }
 
  $all_info=new Students();
  $all_info->address="dhaka";
     echo $all_info->address;
+
 
 ?>
