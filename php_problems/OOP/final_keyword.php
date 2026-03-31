f4edb990fb6798aaf5420101c3afcf10b18bb0ab
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-31 10:38:00 +0600

    Basic understandin final keyword concept.

--- php_problems/OOP/final_keyword.php
+++ php_problems/OOP/final_keyword.php
@@ -1,13 +1,22 @@
-<?php
-class A{
-    public function(){
-        echo ("Hi, I'm Mahabub Alam");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final keyword</title>
+</head>
+<body>
+    <?php
+    final class Animal {
+    public function sound() {
+        echo "Animal sound";
     }
 }
 
-class Name{
-    public $name="Mahabub"
+// Error: Cannot extend final class Animal  
+class Dog extends Animal {
 }
 
-
 ?>
+</body>
+</html>
