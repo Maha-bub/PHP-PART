91f10e0702eec1168bfec52401cc781bfabf6629
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-01 11:07:26 +0600

    PHP file read operation

--- File_read_write/file-read.php
+++ File_read_write/file-read.php
@@ -1,5 +1,7 @@
 <?php
-$open = fopen("file.txt", "r") or die("Unable to open file!");
-echo fread($open, filesize("file.txt"));    
-fclose($open);          
+// $open = fopen("file.txt", "r") or die("Unable to open file!"); die method is used for error handling. 
+// echo fread($open, filesize("file.txt"));    
+// fclose($open);    
+
+$document=fopen("file.txt","r") or die("Unable to open file!");
 ?>
