f715a25007c3fb7a8567bc1b47033960c7e2cd11
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-01 11:40:11 +0600

    PHP file read operation

--- File_read_write/file-read.php
+++ File_read_write/file-read.php
@@ -3,9 +3,15 @@
 // echo fread($open, filesize("file.txt"));    
 // fclose($open);    
 
-$document=fopen("file.txt","r") or die("Unable to open file!");
-echo fread($document, filesize("file.txt"));
-fclose($document);
-?>
+// $document=fopen("file.txt","r") or die("Unable to open file!");
+// echo fread($document, filesize("file.txt"));
+// fclose($document);
+
+echo "<br>";
+
+// echo readfile("file.txt");(read file is used to read the file and it also returns the number of bytes read from the file.It is a built_in function in php);
+echo "<br>";
+echo file_get_contents("file.txt");
 
 ?>
+
