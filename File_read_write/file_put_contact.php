4c02edaf8d5953bd12645dcfccae37f97f487169
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-01 12:36:29 +0600

    adding text info using file put contents

--- File_read_write/file_put_contact.php
+++ File_read_write/file_put_contact.php
@@ -1,7 +1,8 @@
 <?php
 $write = file_put_contents("msg.txt", "Hello world! This is a text file.
 Hi, I'm Mahabubul Alam.",FILE_APPEND);
-echo "Data written successfully.";  
+echo "Data written successfully."; 
+echo $write." bytes written to the file."; 
 
 
 
