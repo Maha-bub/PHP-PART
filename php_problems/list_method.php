02f24683a66470add84edf35a8a8f799b7c5d63f
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-02 10:06:01 +0600

    for assign value to the variable and destructuring

--- php_problems/list_method.php
+++ php_problems/list_method.php
@@ -1,15 +1,18 @@
 
     <?php
-    $employee_info=[
-        [1,"Mahabub","alam@gamil.com",+8801755],
-        [2,"Asad","islam@gamil.com",+8801755],
-        [3,"Tanjil","hasan@gamil.com",+88017155],
-        [4,"Polok","palok@gamil.com",+8801755],
+    $employee_info =[
+        [1,"Mahabub","alam@gamil.com","+8801755"],
+        [2,"Asad","islam@gamil.com","+8801755"],
+        [3,"Tanjil","hasan@gamil.com","+88017155"],
+        [4,"Polok","palok@gamil.com","+8801755"],
     ];
 
     // for value assign and array destructuring
 
-    foreach( $value as list($Id,$name,$mail,$contact)){
-        echo "$Id |$name|$age";
+    foreach( $employee_info as list($Id, $name , $mail , $contact)){
+        echo "$Id | $name | $mail"."<br>";
     }
+
+    $data=["Rakib",12,77]
+    // echo $data;
     ?>
