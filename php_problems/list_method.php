c0bb12a032b5ec2ff2651cd1d6d5b524616a2cb8
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-02 10:56:37 +0600

    for assign value to the variable and destructuring

--- php_problems/list_method.php
+++ php_problems/list_method.php
@@ -1,18 +1,28 @@
 
     <?php
-    $employee_info =[
-        [1,"Mahabub","alam@gamil.com","+8801755"],
-        [2,"Asad","islam@gamil.com","+8801755"],
-        [3,"Tanjil","hasan@gamil.com","+88017155"],
-        [4,"Polok","palok@gamil.com","+8801755"],
-    ];
+    // $employee_info =[
+    //                     [1,"Mahabub","alam@gamil.com","+8801755"] ,
+    //                     [2,"Asad","islam@gamil.com","+8801755"] ,
+    //                     [3,"Tanjil","hasan@gamil.com","+88017155"] ,
+    //                     [4,"Polok","palok@gamil.com","+8801755"] 
+    //                 ];
+
+    $employee_info = [
+    "1,Mahabub,alam@gmail.com,+8801755",
+    "2,Asad,islam@gmail.com,+8801755",
+];
 
     // for value assign and array destructuring
 
-    foreach( $employee_info as list($Id, $name , $mail , $contact)){
-        echo "$Id | $name | $mail"."<br>";
-    }
+    // foreach( $employee_info as $value){
+    //     list($Id, $name , $mail , $contact)=explode(",",$value);
+    //     echo "$Id | $name | $mail"."<br>";
+    // }
 
-    $data=["Rakib",12,77]
-    // echo $data;
+    foreach($employee_info as $value){
+    list($Id, $name , $mail , $contact) = explode(",", $value);
+    echo "$name | $mail |$contact | $Id"."<br>";
+}
+
+    
     ?>
