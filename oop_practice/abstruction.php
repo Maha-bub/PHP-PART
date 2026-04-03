24ce2fff4eb52c3d2987133583cf6d47cfc37114
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-03 13:49:24 +0600

    Abstract essential data from a abstruct class

---
+++ oop_practice/abstruction.php
@@ -0,0 +1,20 @@
+<?php
+ abstract class Shape{
+    abstract public function area();
+
+ }
+
+  class Circle extends Shape{
+    private $radius=10;
+    private $pi=3.1416;
+    
+    public function area(){
+        $area=2*$this->pi * $this->radius * $this->radius;
+        return $area;
+    }
+    }
+    $result=new Circle();
+    echo $result->area();
+
+
+?>
