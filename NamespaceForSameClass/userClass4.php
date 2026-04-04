8738db968b8168af07d3bf718308c63cb0be811f
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-04 10:50:49 +0600

    using namespace keyword for user same class

---
+++ NamespaceForSameClass/userClass4.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UserFour;
+
+class User{
+    public $name="Liyes";
+    public $designation="Jr. Developer";
+
+
+    public function userInfo(){
+        echo "Hi, I'm ".$this->name." working as a ".$this->designation;
+    }
+}
+
+
+?>
