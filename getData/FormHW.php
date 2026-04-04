39395823e630f7028c52a2ff91c74c4865802fd5
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-04 09:43:22 +0600

    form  data access

--- getData/FormHW.php
+++ getData/FormHW.php
@@ -3,35 +3,31 @@
 class User_Input {
     public $id;
     public $name;
-    public static $fileName="store.txt";
+    public static $fileName = "store.txt";
 
-     public function __construct($id, $name) {
+    public function __construct($id, $name) {
         $this->id = $id;
         $this->name = $name;
     }
 
-    public function Child(){
-        return $this->id."," . $this->name."\n"; 
+    public function child() {
+        return $this->id . "," . $this->name . "\n";
     }
 
-    public function saveToFile(){
-        file_put_contents(self::$fileName, $this->Child(), FILE_APPEND);
-  
-    }  
- } 
-  
-?>
+    public function saveToFile() {
+        file_put_contents(self::$fileName, $this->child(), FILE_APPEND);
+    }
+}
 
-<?php
-if(isset($_POST['submit']) ){
-
+if (isset($_POST['submit'])) {
     $userInputId = $_POST['id'];
     $userInputName = $_POST['name'];
+
     $userInput = new User_Input($userInputId, $userInputName);
     $userInput->saveToFile();
 }
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -41,10 +37,10 @@ if(isset($_POST['submit']) ){
     <form method="POST" action="">
         <label for="id">ID:</label>
         <input type="text" id="id" name="id" required><br><br>
-        
+
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br><br>
-        
+
         <button type="submit" name="submit">Submit</button>
     </form>
 </body>
--- php_problems/list_method.php
+++ php_problems/list_method.php
@@ -1,18 +1,18 @@
 
     <?php
-    // $employee_info =[
-    //                     [1,"Mahabub","alam@gamil.com","+8801755"] ,
-    //                     [2,"Asad","islam@gamil.com","+8801755"] ,
-    //                     [3,"Tanjil","hasan@gamil.com","+88017155"] ,
-    //                     [4,"Polok","palok@gamil.com","+8801755"] 
-    //                 ];
+    $employee_info =[
+                        [1,"Mahabub","alam@gamil.com","+8801755"] ,
+                        [2,"Asad","islam@gamil.com","+8801755"] ,
+                        [3,"Tanjil","hasan@gamil.com","+88017155"] ,
+                        [4,"Polok","palok@gamil.com","+8801755"] 
+                    ];
 
-     // for value assign and array destructuring
+     for value assign and array destructuring
 
-    // foreach( $employee_info as $value){
-    //     list($Id, $name , $mail , $contact)=explode(",",$value);
-    //     echo "$Id | $name | $mail"."<br>";
-    // }
+    foreach( $employee_info as $value){
+        list($Id, $name , $mail , $contact)=explode(",",$value);
+        echo "$Id | $name | $mail"."<br>";
+    }
 
     $employee_info = [
     "1,Mahabub,alam@gmail.com,+8801755",
