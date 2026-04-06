b41ae3d73c9267c6cf17b5d0383cb7e6a6ee3dfd
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-06 10:56:16 +0600

    using reaplace for prag_match_replace

--- Regular Expression/preg_function.php
+++ Regular Expression/preg_function.php
@@ -1,18 +1,22 @@
 <?php
 // pattern/modifier/(case-sensitive or insensitive);
-//preg_match
-//preg_match_all()
-//preg_replace
+// preg_match
+// preg_match_all()
+// preg_replace
 
 $str="Hi!, I'm Mahabub Alam as a trainee at IsDB BISEW IT  Scholourship Programm.";
 $pattern="/i/i";
 
-//prag match work to show the result count once.
+// prag match work to show the result count once.;
 echo preg_match($pattern,$str);
 
 // prag match all is work for show the modifire total count.
 echo preg_match_all($pattern ,$str);
+echo "<br>";
 
+$descrp="Working as a Senior Software Engineer.";
+$syntexPattern="/Senior/";
+echo preg_replace($syntexPattern,"Junior",$descrp);
 
 
 
