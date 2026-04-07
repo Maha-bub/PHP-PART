6b2bb726188340735df78f8ae4d5e86cf0e8d67d
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-07 12:02:04 +0600

    check Data validation using pattern and preg_function_all() expression

--- Regular Expression/validationUserdata.php
+++ Regular Expression/validationUserdata.php
@@ -1,8 +1,19 @@
 <?php
 //  mail validation example;
-$userMail="mahabubulalam0511@gmail.com";
-$pettern="/^[a-zA-Z0-9._+%-]+@[A-Za-z0-9]+\.[a-zA-Z]{2,}$/";
-echo preg_match_all($pettern,$userMail);
+$userMail="mahabubulalam0511@gmail.c321om";
+$pettern="/^[a-zA-Z0-9._+%-]+@gmail\.[a-zA-Z0-9]{2,}$/";
+if(preg_match_all($pettern,$userMail)){
+    echo "valid mail";
+}
+else{
+    echo "invalid mail";
+}
+
+
+
+// $userMail="mahabubulalam0511@gmail.com";
+// $pettern="/^[a-zA-Z0-9._+%-]+@[A-Za-z0-9]+\.[a-zA-Z]{2,}$/";
+// echo preg_match_all($pettern,$userMail);
 
 
 ?>
