a340bd08087cb20b10dda3ecf55be631a9f9e6b4
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-07 11:49:31 +0600

    check Data validation using pattern and preg_function_all() expression

--- Regular Expression/pregMatchValidation.php
+++ Regular Expression/pregMatchValidation.php
@@ -1,9 +1,12 @@
 <?php
 // preg match all
 requrment:
-$data="akashAlam214";
-$pattern="/^[A-Za-z0-9]{3,13}$/";
-echo preg_match_all($pattern,$data);
+// $data="akashAlam214";
+// $pattern="/^[A-Za-z0-9]{3,13}$/";
+// echo preg_match_all($pattern,$data);
+$data="01304681301";
+$requirement="/^01[3-9][0-9]{8}$/";
+echo preg_match_all($requirement,$data);
 
 
 
