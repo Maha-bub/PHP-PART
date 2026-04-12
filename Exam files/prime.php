858e80bdb58efa97954fd2b8fefeff499835c0ed
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-12 12:21:00 +0600

    find student info by search id

--- Exam files/prime.php
+++ Exam files/prime.php
@@ -7,39 +7,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $count = 1;
     }
 
-    for ($i = 2; $i < $number; $i++) {
+    for ($i = 2; $i < $number/2; $i++) {
         if ($number % $i == 0) {
             $count = 1;
             break;
         }
-    }
-
-
-    
+    }    
     if ($count == 0) {
         echo "This is a prime number.";
     } else {
         echo "This is not a prime number.";
-}
+    }
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <title>Check Prime Number</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find the largest number among three numbers.</title>
+    <link rel="stylesheet" href="largest.css">
+
 </head>
-
 <body>
-
-    <form action="" method="post">
-        Enter a number:<br>
-        <input type="number" name="number"><br><br>
-        <input type="submit" value="Submit">
+    <form action="#" method="POST">
+        <h1>Enter A Number:</h1>
+        <input type="number" name="number" placeholder="Number"> 
+        <input type="submit" name="submit" value="Submit">
     </form>
-
 </body>
-
 </html>
