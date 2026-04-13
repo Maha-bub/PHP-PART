91ea6465132a55aeee1c546085404e3b6bf344d5
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-13 12:12:21 +0600

    data encryption use base64_encode and base64_decode method

--- Encryption_decryption/encrypt_encode.php
+++ Encryption_decryption/encrypt_encode.php
@@ -10,4 +10,25 @@ echo "<br>";
 // base64_decode use for decrypt from encrypted data
 echo base64_decode('MTIzMTIzMQ==');
 
+
+
+echo "<br>";echo "<br>";echo "<br>";
+$PasswordData="alam";
+$userKey="2a123";
+$build_in_method="AES-128-CTR";
+
+
+// // iv use for unique encryption value. if we don't use iv then every single encrypted value are same, so we use IV for extra security 
+
+
+// // iv (Initialization vector) means extra security layer
+// $iv_length = openssl_cipher_iv_length($build_in_method);
+
+// // random IV (Initialization vector)
+// $iv = openssl_random_pseudo_bytes($iv_length);
+
+
+$encrypted=openssl_encrypt($PasswordData, $build_in_method, $userKey, 0,$iv);
+echo $encrypted;
+
 ?>
