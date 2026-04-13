606cf2f280577f7a09227478ca126244764a3c0e
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-13 11:40:36 +0600

    data encryption and using hashing function

--- Encryption_decryption/Encryption.php
+++ Encryption_decryption/Encryption.php
@@ -1,26 +1,31 @@
 <?php
 $password="123123";
-echo md5($password);
+// echo md5($password);
 
 
-echo "<br>";
-echo sha1($password);
-echo "<br>";
-echo hash ('sha224',$password);
-echo "<br>";
+// echo "<br>";
+// echo sha1($password);
+// echo "<br>";
+// echo hash ('sha224',$password);
+// echo "<br>";
 
-echo hash('sha3-512',$password);
-echo "<br>";
-echo hash('sha3-384',$password);
-echo "<br>";
-echo hash('sha512/224',"mahabub                                           0                                                                                                                                                                                                                                                                                                                                                                      ");
-echo "<br>";
- $encrypt=hash('sha512',$password);
-echo "<br>";
-echo "<br>";
-echo "<br>";
+$new= hash('sha3-512',$password);
+// echo "<br>";
+// echo hash('sha3-384',$password);
+// echo "<br>";
+// echo hash('sha512/224',"mahabub                                           0                                                                                                                                                                                                                                                                                                                                                                      ");
+// echo "<br>";
 
-password_hash($encrypt, PASSWORD_DEFAULT);
+
+
+ $encrypt=password_hash($password,PASSWORD_DEFAULT);
+ echo $encrypt;
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$password="123123";
+
+// password_hash($encrypt, PASSWORD_DEFAULT);
  if(password_verify($password,$encrypt)){
     echo "valid";
 
